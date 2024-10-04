1bcf9b4fc2e37987e55b418bd51c5bd0e78629f8
Harshavardhan0727 <kasiharshav2@gmail.com>
2024-10-04 13:31:06 -0700

    Fix 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import AOS from 'aos'; // Import AOS library
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import 'aos/dist/aos.css'; // Import AOS styles
 import './App.css';
 
 function App() {
+  useEffect(() => {
+      AOS.init({
+          duration: 1000, // Animation duration
+          delay: 200, // Delay between animations
+          once: false // Ensures animation happens every time the element comes into view
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div>
+            <Header />
+            <Hero />
+            <About />
+            <Projects />
+            <Contact />
+            <Footer />
+        </div>
+    );
+};
 
 export default App;
