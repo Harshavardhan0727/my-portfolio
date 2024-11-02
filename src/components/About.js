aaeafd5166d56f420f9fe677e4f6aec302b1340b
Harshavardhan0727 <kasiharshav2@gmail.com>
2024-11-02 15:44:05 -0700

    About changes

--- src/components/About.js
+++ src/components/About.js
@@ -7,10 +7,7 @@ const About = () => {
         <section id="about" data-aos="fade-right">
             <h2 data-aos="fade-up">About Me</h2>
             <p data-aos="fade-up" data-aos-delay="200">
-            Results-oriented Full-stack Java Developer with 5+ years of experience in Java, Spring Boot, and JavaScript frameworks.
-            Proficient in SQL and NoSQL databases, with expertise in Git, RESTful APIs, and microservices architecture.
-            Skilled inHTML5, CSS3, and responsive design. Track record optimizing code for performance and troubleshooting.
-             Well-versed inAgile methodologies and CI/CD pipelines
+            Dynamic and results-driven Java Full Stack Developer with over 5 years of comprehensive experience in designing, developing, and deploying robust applications. Proficient in core Java and JavaScript, with advanced expertise in front-end frameworks such as React and Angular. Skilled in leveraging AWS cloud services to enhance application performance and scalability. Proven ability to implement end-to-end solutions, utilizing RESTful APIs and microservices architecture. Adept at collaborating with cross-functional teams to deliver high-quality software in Agile environments. Passionate about driving innovation and optimizing user experiences through effective technology integration.
             </p>
         </section>
     );
