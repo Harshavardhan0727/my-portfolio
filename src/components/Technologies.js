5ea44519758cfb7e74996054be0da8df6d44a0c0
Harshavardhan0727 <kasiharshav2@gmail.com>
2024-11-02 15:27:17 -0700

    Tech Changes

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaJava, FaReact, FaGithub, FaAws, FaDocker } from 'react-icons/fa';
+import { FaJava, FaReact, FaGithub, FaAws, FaDocker, FaHtml5, FaCss3Alt, FaJsSquare } from 'react-icons/fa';
 import { SiSpringboot, SiMongodb, SiKubernetes, SiAngular } from 'react-icons/si';
 import './Technologies.css';
 
@@ -21,26 +21,38 @@ const Technologies = () => {
                     <p>React</p>
                 </div>
                 <div className="icon-container" data-aos="fade-up" data-aos-delay="300">
+                    <FaHtml5 className="icon html-icon" />
+                    <p>HTML</p>
+                </div>
+                <div className="icon-container" data-aos="fade-up" data-aos-delay="400">
+                    <FaCss3Alt className="icon css-icon" />
+                    <p>CSS</p>
+                </div>
+                <div className="icon-container" data-aos="fade-up" data-aos-delay="500">
+                    <FaJsSquare className="icon js-icon" />
+                    <p>JavaScript</p>
+                </div>
+                <div className="icon-container" data-aos="fade-up" data-aos-delay="600">
                     <SiSpringboot className="icon springboot-icon" />
                     <p>Spring Boot</p>
                 </div>
-                <div className="icon-container" data-aos="fade-up" data-aos-delay="400">
+                <div className="icon-container" data-aos="fade-up" data-aos-delay="700">
                     <SiMongodb className="icon mongodb-icon" />
                     <p>MongoDB</p>
                 </div>
-                <div className="icon-container" data-aos="fade-up" data-aos-delay="500">
+                <div className="icon-container" data-aos="fade-up" data-aos-delay="800">
                     <FaGithub className="icon github-icon" />
                     <p>GitHub</p>
                 </div>
-                <div className="icon-container" data-aos="fade-up" data-aos-delay="600">
+                <div className="icon-container" data-aos="fade-up" data-aos-delay="900">
                     <FaAws className="icon aws-icon" />
                     <p>AWS</p>
                 </div>
-                <div className="icon-container" data-aos="fade-up" data-aos-delay="700">
+                <div className="icon-container" data-aos="fade-up" data-aos-delay="1000">
                     <SiKubernetes className="icon kubernetes-icon" />
                     <p>Kubernetes</p>
                 </div>
-                <div className="icon-container" data-aos="fade-up" data-aos-delay="800">
+                <div className="icon-container" data-aos="fade-up" data-aos-delay="1100">
                     <FaDocker className="icon docker-icon" />
                     <p>Docker</p>
                 </div>
